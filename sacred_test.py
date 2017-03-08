adf459de072453de9b96c043acab36154c0ab807
Lukas Faber <lukas.faber@student.hpi.de>
2017-03-08 16:02:53 +0100

    build cross product of parameter plus example config file with echo

--- sacred_test.py
+++ sacred_test.py
@@ -12,11 +12,24 @@ def config():
 def main(cfgfile):
     config = json.load(open(cfgfile))
     cmd_string = "{0} {1} {2}".format(
-        "" if config["env"] is None else config["env"],
-        "" if config["path"] is None else "cd " + config["path"] + "&& ",
+        "" if config["env"] == "" else config["env"],
+        "" if config["path"] =="" else "cd " + config["path"] + "&& ",
         config["cmd"])
-    cmd_string += " " + config["params"][0]["value"][0]
-    print(cmd_string)
-    subprocess.call(cmd_string,shell=True)
-
-#R_HOME=/usr/lib/R;LD_LIBRARY_PATH=/usr/lib/R/lib:/usr/lib/R/bin:/home/lukas/R/x86_64-pc-linux-gnu-library/3.2/rJava/jri:;R_SHARE_DIR=/usr/share/R/share;R_INCLUDE_DIR=/usr/share/R/include;R_DOC_DIR=/usr/share/R/doc
+    params = config["params"]
+    if len(params) <= 1:
+        cmd_string += " " + config["params"][0]["value"][0]
+        subprocess.call(cmd_string,shell=True)
+    else:
+        paramlist = [[x] for x in params[0]["value"]]
+        print(paramlist)
+        for param in params[1:]:
+            values = param["value"]
+            paramlist2 = []
+            for v in values:
+                for p in paramlist:
+                    paramlist2.append(p + [v])
+            paramlist = paramlist2
+        print(paramlist)
+        for par in paramlist:
+            cmd_par = cmd_string + " " + " ".join(par)
+            subprocess.call(cmd_par,shell=True)
