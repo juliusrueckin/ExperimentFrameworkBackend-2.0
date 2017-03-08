4b232a388285c7123c60ff2516805f0f780b9f05
Lukas Faber <lukas.faber@student.hpi.de>
2017-03-08 15:32:58 +0100

    specify path,env,cmd,params in separate file plus adding example file

--- sacred_test.py
+++ sacred_test.py
@@ -1,10 +1,22 @@
 from sacred import Experiment
+import json
 import subprocess
 
 ex = Experiment('sacred_test')
 
+@ex.config
+def config():
+    cfgfile=None
+
 @ex.automain
-def main():
-    subprocess.call("cd /home/lukas/work/MP2016MUE/graphdiff/ && echo $PWD && java -Xmx4G -jar target/graphdiff-1.0-SNAPSHOT.jar Brad_Pitt George_Clooney Leonardo_DiCaprio Scarlett_Johansson Johnny_Depp",shell=True)
+def main(cfgfile):
+    config = json.load(open(cfgfile))
+    cmd_string = "{0} {1} {2}".format(
+        "" if config["env"] is None else config["env"],
+        "" if config["path"] is None else "cd " + config["path"] + "&& ",
+        config["cmd"])
+    cmd_string += " " + config["params"][0]["value"][0]
+    print(cmd_string)
+    subprocess.call(cmd_string,shell=True)
 
 #R_HOME=/usr/lib/R;LD_LIBRARY_PATH=/usr/lib/R/lib:/usr/lib/R/bin:/home/lukas/R/x86_64-pc-linux-gnu-library/3.2/rJava/jri:;R_SHARE_DIR=/usr/share/R/share;R_INCLUDE_DIR=/usr/share/R/include;R_DOC_DIR=/usr/share/R/doc
